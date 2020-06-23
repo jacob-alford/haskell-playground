87df3cc48cebabe1a6308060cc67caebac42d452
Jacob Alford - MBP <jacob.alford@me.com>
2020-06-23 14:30:12 -0600

    Init w/ diag diff

---
+++ diagonal-difference.hs
@@ -0,0 +1,23 @@
+shape :: Int -> [a] -> [[a]]
+shape _ [] = []
+shape n arr
+  | length arr < n  = error "Provided shape does not match array dimensions!"
+  | length arr == n = [] ++ [take n arr] ++ shape n (drop n arr)
+  | otherwise       = [take n arr] ++ shape n (drop n arr)
+
+offDiag :: Int -> [[a]] -> a
+offDiag index mat = mat !! index !! offIndex
+  where offIndex = (length mat - 1) - index
+
+diag :: Int -> [[a]] -> a
+diag rowCol mat = mat !! rowCol !! rowCol
+
+getDiags :: [[a]] -> [a]
+getDiags matrix = [ ith `diag` matrix | (_, ith) <- matrix `zip` [0 ..] ]
+
+getOffDiags :: [[a]] -> [a]
+getOffDiags matrix = [ ith `offDiag` matrix | (_, ith) <- matrix `zip` [0 ..] ]
+
+diagDiff :: [[Int]] -> Int
+diagDiff matrix = abs $ (sum $ getDiags matrix) - (sum $ getOffDiags matrix)
+
