94624518ded8758b69a94ae758fbd080bfc290bc
Jacob Alford - MBP <jacob.alford@me.com>
2020-07-18 14:08:02 -0600

    Fix apple but gosh dangit algorithm

--- sumOfIntervals.hs
+++ sumOfIntervals.hs
@@ -6,13 +6,23 @@ where
 import           Data.List                      ( sort )
 
 joint :: (Int, Int) -> (Int, Int) -> Bool
-joint (a1, a2) (b1, b2) = (b1 > a1 && b1 < a2) || (b2 < a2 && b2 > a1)
+joint (a1, a2) (b1, b2) =
+  (a1 == b1) || (a2 == b2) || (b1 > a1 && b1 < a2) || (b2 < a2 && b2 > a1)
 
 properIntv :: (Int, Int) -> (Int, Int) -> (Int, Int)
 properIntv (a1, a2) (b1, b2) = (min a1 b1, max a2 b2)
 
 reduceToDisjoint :: [(Int, Int)] -> [(Int, Int)]
-reduceToDisjoint intvs = foldl (\acc next -> if joint (last acc) next then (acc) : (properIntv (last acc) (next)) else )
+reduceToDisjoint intvs = foldl
+  (\djs nxt -> if joint (head djs) nxt
+    then (properIntv (head djs) nxt) : (tail djs)
+    else nxt : djs
+  )
+  [head intvs]
+  (tail intvs)
+
+sumIntvs :: [(Int, Int)] -> Int
+sumIntvs = foldl (\total (low, high) -> total + (high - low)) 0
 
 sumOfIntervals :: [(Int, Int)] -> Int
-sumOfIntervals intervals = undefined
+sumOfIntervals = sumIntvs . reduceToDisjoint . reverse . sort
